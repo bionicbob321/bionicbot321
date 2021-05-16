5c09244234add3594e87fc7659410bf67a0f65f0
bionicbob321 <82731973+bionicbob321@users.noreply.github.com>
2021-05-16 13:31:59 +0100

    Update 1.3.0
    Check changelog.txt for changes. Made some back end improvements to performance. Also rewrote and removed Deprecated code, and tidied up comments to improve readability.

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const config = require("./config.json"); //config file
 const fs = require("fs"); //file system
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
+//Read strings from file
+
 const commandsDesc = fs.readFileSync("commands.txt");
 const changelog = fs.readFileSync("changelog.txt");
 const insults = fs.readFileSync("insults.txt").toString(`utf-8`);
 const insultsArray = insults.split(`&&&`);
 const susASCII1 = fs.readFileSync("imposter1.txt").toString(`utf-8`);
 const susASCII2 = fs.readFileSync("imposter2.txt").toString(`utf-8`);
+
+//Discord client defs 
 
 const client = new Discord.Client(); //starts client
 client.login(config.BOT_TOKEN); //grabs token from config.json and logs in 
@@ -19,6 +23,92 @@ client.on("ready", function(ready) { //runs the following code when the bot is f
     });
     console.log("Bot is ready");
 });
+
+//
+//Runtime function definitions
+//
+
+function makeRequest(url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest;
+        xhr.open("GET", url);
+        function resolvePromise () {
+            //if (status == 200) { //status isnt defined anywhere. This throws an error and script ends without error message cuz node does node things
+            resolve(JSON.parse(xhr.responseText));
+            //} else {
+            reject(); //not sure if this works. need to check
+            //};
+        };
+        xhr.addEventListener("load", resolvePromise);
+        xhr.send();
+        
+    });     
+};
+
+async function redditRequest(subreddit, postNumber) { //temporarily dont allow postFilters cuz i cba and im sick of this shit just make it stop please just make it stop im losing my fucking mind here please just make it stop
+    //need to properly validate postFilter because it will matter later
+    let postFilter = "top"; //TEMP - see above
+    let redditURL = (`https://www.reddit.com/r/${subreddit}/${postFilter}.json`);
+    let result = await makeRequest(redditURL);
+    if (typeof postNumber == Number) {postNumber -= 1};
+    if (postNumber == "r" || postNumber == "rand" || postNumber == "random" || postNumber == undefined || postNumber > 24 || postNumber < 0 || typeof postNumber != Number) {
+        postNumber = (Math.floor((Math.random() * 25) + 1)) - 1;
+    };
+    let postData = result["data"]["children"][postNumber]["data"];
+    if (!postData.hasOwnProperty("selftext")) {
+        return (`**${postData.title}**:\n\n${postData.url}`);
+    } else if (postData.hasOwnProperty("selftext")) { //only text posts have the selftext property.
+        return (`**${postData.title}**:\n\n${postData.selftext}\n\n${postData.url}`);
+    } else {
+        return ("Unknow error. Please try again later");
+    };
+};
+
+function calculate(calculationPassed) {
+    let calculation = calculationPassed
+    let calculationSplit = calculation.split(" ");
+    calculation = calculationSplit.join();
+    calculation = calculation.replace(/,/g, ""); // '/,/g' is regex for "match every occourence of ','"
+    let total;
+    let splitCalc; 
+    try { 
+    if (calculation.indexOf("**") > -1) {
+        splitCalc = calculation.split("**");
+        total = parseFloat(splitCalc[0]) ** parseFloat(splitCalc[1])
+        return total;
+    };
+    if (calculation.indexOf("//") > -1) { //make else if to reduce compute load //dont do this bad idea.
+        splitCalc = calculation.split("//");
+        total = Math.pow(parseFloat(splitCalc[0]), 1/parseFloat(splitCalc[1]));
+        return total;
+    }; 
+    if (calculation.indexOf("+") > -1) {
+        splitCalc = calculation.split("+");
+        total = parseFloat(splitCalc[0]) + parseFloat(splitCalc[1]);
+        return total;
+    };
+    if (calculation.indexOf("-") > -1) {
+        splitCalc = calculation.split("-");
+        total = parseFloat(splitCalc[0]) - parseFloat(splitCalc[1]);
+        return total;
+    };
+    if (calculation.indexOf("/" > -1)) { //I have no fucking clue why but this only works if placed above the multiply section
+        splitCalc = calculation.split("/");
+        total = parseFloat(splitCalc[0]) / parseFloat(splitCalc[1]);
+        return total;
+    };
+    if (calculation.indexOf("*" > -1)) {
+        splitCalc = calculation.split("*");
+        total = parseFloat(splitCalc[0]) * parseFloat(splitCalc[1]);
+        return total;
+    };
+    return 'INVALID CALCULATION. Do the "calchelp" command for help.';
+    } catch (error) {
+        console.log(error);
+        return 'INVALID CALCULATION. Do the "calchelp" command for help.';
+    };
+};
+
 
 const database = JSON.parse(fs.readFileSync("database.json"));
 function save() {
@@ -36,13 +126,17 @@ function save() {
 };
 save();
 
+//
+//Message response def
+//
+
 client.on("message", function(message) { //sets event listener for messages sent in a server    
     if (message.author.bot) return; //checks if user is a bot, and returns nothing if true
 
     if (message.mentions.users.first() != null) { //checks if the message contains any mentions
         if (message.mentions.users.first().username == "bionicbot321" && message.mentions.users.first().bot == true) { //checks if the mentioned user has username "BionicBot321" and is a bot
             message.reply('What the fuck did you just fucking say about me, you little bitch? I will have you know I graduated top of my class in the Navy Seals, and I have been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I am the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You are fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that is just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little "clever" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you could not, you did not, and now you are paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You are fucking dead, kiddo.');
-        };
+        }; //move this to a text file
     } //The guy who wrote the code above is an idiot
     
     let guild = message.guild.id; //guild = the guild id of the message
@@ -54,7 +148,7 @@ client.on("message", function(message) { //sets event listener for messages sent
 
     function sendMessage(messageContent) {
         if (content = undefined) return;
-        message.channel.send(messageContent);
+        message.channel.send(messageContent, { split: true });
     };
 
     function mention() {
@@ -111,8 +205,14 @@ client.on("message", function(message) { //sets event listener for messages sent
             let tempArgs = args;
             tempArgs.shift();
             let joinedArgs = tempArgs.join(" ")
-            for (let I=0; I < 10; I++) {
-                sendMessage("<@" + mention().id + ">, " + joinedArgs);
+            if (joinedArgs != "") {
+                for (let I=0; I < 10; I++) {
+                    sendMessage("<@" + mention().id + ">, " + joinedArgs);
+                };
+            } else {
+                for (let I=0; I < 10; I++) {
+                    sendMessage("<@" + mention().id + ">");
+                };
             };
         break;
         case "help":
@@ -199,5 +299,18 @@ client.on("message", function(message) { //sets event listener for messages sent
         case "sourcecode":
             sendMessage("The source code for this project can be viewed at:\nhttps://github.com/bionicbob321/bionicbot321");
         break;
+        case "reddit":
+            async function redditRequestSendMessage() { //This is the most scuffed piece of code I have ever written but it works so idc
+                sendMessage(await redditRequest(args[0], args[1]));
+            };
+            redditRequestSendMessage(); 
+        break;
+        case "calc":
+            sendMessage(calculate(commandBody.slice(5)));
+        break;
+        case "calculate":
+            sendMessage(calculate(commandBody.slice(10)));
+        break;
+            
     };
 });
